2fa752684ea36e58631a59c02b8a1657f96c9e85
AzStowe <aaron.z.stowe@gmail.com>
2019-06-13 11:33:06 -0500

    the bird now has toys

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 
 # Create your views here.
@@ -30,11 +29,13 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  # instantiate FeedingForm to be rendered in the template
+  # Get the toys the finch doesn't have
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
-    # pass the cat and feeding_form as context
-    'finch': finch, 'feeding_form': feeding_form
+    'finch': finch, 'feeding_form': feeding_form,
+    # Add the toys to be displayed
+    'toys': toys_finch_doesnt_have
   })
 
 def add_feeding(request, finch_id):
@@ -48,3 +49,30 @@ def add_feeding(request, finch_id):
     new_feeding.finch_id = finch_id
     new_feeding.save()
   return redirect('detail', finch_id=finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
